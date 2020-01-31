3dbc54e564be6075ce2b3507f1ce91061b90494b
Connor Crossley <ccrossley@cbtnuggets.com>
2020-01-31 13:03:16 -0800

    add routing for menu items

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
-import React from "react";
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+import HomePage from './pages/homepage/homepage.component';
 
-import HomePage from "./pages/homepage/homepage.component";
-
-function App() {
-  return (
+const HatsPage = () => (
     <div>
-      <HomePage />
+        <h1>HATS PAGE</h1>
     </div>
-  );
+);
+function App() {
+    return (
+        <div>
+            <Switch>
+                <Route exact path='/' component={HomePage} />
+                <Route path='/shop/hats' component={HatsPage} />
+            </Switch>
+        </div>
+    );
 }
 
 export default App;
